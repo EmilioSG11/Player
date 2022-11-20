25be380a4ff1e2e33a67c58907fff06a8f07fc88
EmilioSG11 <112224145+EmilioSG11@users.noreply.github.com>
2022-11-20 07:04:32 -0600

    Create Settings.js

---
+++ Settings/Settings.js
@@ -0,0 +1,42 @@
+var player = videojs('emsgplayer1', {
+	autoplay: '',
+	controls: true,  
+  liveui: true,
+  fluid: true,
+  preload: 'auto',  
+	loop: false,
+	playbackRates: [0.25, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+  html5:
+  {
+    vhs: {
+    enableLowInitialPlaylist: false,
+      fastQualityChange: true,
+      useBandwidthFromLocalStorage: true,
+      overrideNative: true,
+      useDevicePixelRatio: true,
+      useNetworkInformationApi: true,      
+    }
+  },
+poster:'https://content.jwplatform.com/thumbs/AeaxqVcs-1280.jpg',
+  plugins: {    
+	}
+});
+
+var player = videojs('emsgplayer1', player);
+  //player.play();
+var player = videojs('emsgplayer1');
+  player.qualityLevels();
+let toggleQuality = (function() {
+  let enable720 = true;
+  return function() {
+    for (var i = 0; i < qualityLevels.length; i++) {
+      let qualityLevel = qualityLevels[i];
+      if (qualityLevel.height >= 720) {
+        qualityLevel.enabled = enable720;
+      } else {
+        qualityLevel.enabled = !enable720;
+      }
+    }
+    enable720 = !enable720;
+  };
+})();
